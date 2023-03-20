6660779e5ac2459da749422b6a9808932faf8c27
Janis <76683226+saschazesiger@users.noreply.github.com>
2023-03-20 20:21:50 +0000

    update path

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -30,4 +30,4 @@ echo "---Starting Chrome---"
 mkdir ${DATA_DIR}
 cd ${DATA_DIR}
 ls
-/usr/bin/chromium ${URL} --auto-hide-toolbar --bwsi  --user-data-dir=${DATA_DIR} --disable-accelerated-video --disable-gpu --no-sandbox --disable-features=ShowSyncUINavigationButton --test-type --dbus-stub ${EXTRA_PARAMETERS} --disable-dev-shm-usage>/dev/null
+/usr/bin/google-chrome ${URL} --auto-hide-toolbar --bwsi  --user-data-dir=${DATA_DIR} --disable-accelerated-video --disable-gpu --no-sandbox --disable-features=ShowSyncUINavigationButton --test-type --dbus-stub ${EXTRA_PARAMETERS} --disable-dev-shm-usage>/dev/null
