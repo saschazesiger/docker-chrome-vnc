d79e8411e90d3ed540f8ee4713cfc5704d8111e8
Janis <76683226+saschazesiger@users.noreply.github.com>
2023-03-21 08:45:00 +0000

    no ARGS

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -30,4 +30,4 @@ echo "---Starting Chrome---"
 mkdir ${DATA_DIR}
 cd ${DATA_DIR}
 ls
-/usr/bin/google-chrome ${URL} --no-default-browser-check --no-first-run --bwsi  --user-data-dir=${DATA_DIR} --disable-gpu-sandbox --disable-infobars --no-sandbox --disable-accelerated-video --disable-gpu --dbus-stub ${EXTRA_PARAMETERS} --disable-dev-shm-usage>/dev/null
+/usr/bin/google-chrome ${URL}  --user-data-dir=${DATA_DIR}  ${EXTRA_PARAMETERS} --disable-dev-shm-usage>/dev/null
